22d6fc356dd8c8576ca67221830c25f981a85460
furkanatesc <furkanatesc@hotmail.com>
2025-09-04 17:50:05 +0300

    Required

---
+++ Northwind.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün ismi boş olamaz");
+            RuleFor(p => p.CategoryId).NotEmpty();
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.QuantityPerUnit).NotEmpty();
+            RuleFor(p => p.UnitsInStock).NotEmpty();
+
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p=>p.UnitsInStock).GreaterThanOrEqualTo((short)0);
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(propa => propa.CategoryId == 2);
+
+            RuleFor(p => p.ProductName).Must(StartWithA);
+
+           
+
+            
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
